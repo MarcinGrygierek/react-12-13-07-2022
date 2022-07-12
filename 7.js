b481ddb3d5918f45d0c9a3f26e0cc43de9fb76fb
Marcin Grygierek <mg@itdotfocus.com>
2022-07-12 12:08:59 +0200

    js + first js exercise

---
+++ 7.js
@@ -0,0 +1,20 @@
+// tworzenie obiektu - wersja es5
+const name = 'Marcin';
+const lastName = 'Grygierek';
+
+const user = {
+  name: name,
+  lastName: lastName,
+};
+
+// użycie "shorthand properties" z es6
+const user2 = {
+  name,
+  lastName,
+};
+
+// interpolacja
+const value = `Ania ma ${2 + 4 / 2} koty`;
+
+// klasy es5 vs es6
+// https://medium.com/@apalshah/javascript-class-difference-between-es5-and-es6-classes-a37b6c90c7f8
---
+++ js_zad_1.js
@@ -0,0 +1,41 @@
+// function getSum(arr) {
+//   var sum = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// v1
+const getSum = (arr) => {
+  let sum = 0;
+
+  arr.forEach((el) => {
+    sum += el;
+  });
+
+  return sum;
+};
+
+// v2
+const getSumV2 = (arr) => {
+  const sum = arr.reduce((acc, curr) => {
+    return acc + curr;
+  });
+
+  return sum;
+};
+
+// v3
+const getSumV3 = (arr) => {
+  return arr.reduce((acc, curr) => {
+    return acc + curr;
+  });
+};
+
+// v4
+const getSumV4 = (arr) => arr.reduce((acc, curr) => acc + curr);
+
+console.log(getSum([1, 2, 3, 4, 5]));
+console.log(getSum([5, 10, 15, 20]));
+console.log(getSum([2, 10, 9, 11]));
